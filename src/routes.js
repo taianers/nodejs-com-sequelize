8ad2607f49ad67201e24eac70e49607c67a1c12f
Taiane Rodrigues <taianerodrigues.tec99@gmail.com>
2021-01-20 15:18:08 -0300

    correções de quase tudo

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.post('/users', )
+
+module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const routes = require('./routes');
 
-app.listen(port);
-console.log(`Servidor rodando na porta ${port}`)
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3000);
+console.log(`Servidor rodando na porta 3000`);
